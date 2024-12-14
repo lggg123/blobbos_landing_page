f3b636ad6a819a7421b4ddf93ab3a9d44972aede
lggg123 <machinelearner2334@gmail.com>
2024-12-14 12:34:42 -0800

    fix: fixed eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,27 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { json } from "stream/consumers";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript'
+    ],
+    rules: {
+      '@typescript-eslint/no-empty-object-type': 'off',
+      'react/no-unescaped-entities': 'off'
+    }
+  })
+]
 
 export default eslintConfig;
